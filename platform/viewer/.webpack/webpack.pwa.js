484be672ba430982a2456c8526dda1ba77cfb5e1
Aditya Anand <adityaanand53@live.com>
2022-12-14 00:50:22 +0530

    port 8080

--- platform/viewer/.webpack/webpack.pwa.js
+++ platform/viewer/.webpack/webpack.pwa.js
@@ -139,7 +139,7 @@ module.exports = (env, argv) => {
       // https: true,
       hot: true,
       open: true,
-      port: 3000,
+      port: 8080,
       client: {
         overlay: { errors: true, warnings: false },
       },
